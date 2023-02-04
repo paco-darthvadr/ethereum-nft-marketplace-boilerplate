2659db8917927b2dd09359572830896c20f10165
paco-darthvadr <paco@ilien.io>
2023-02-04 18:14:32 +0000

    Changes to quickstart, added home

--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -35,167 +35,139 @@ export default function QuickStart({ isServerInfo }) {
       <Card style={styles.card} title={<h1 style={styles.title}>📝 To-Do List</h1>}>
         <Timeline mode="left" style={styles.timeline}>
           <Timeline.Item dot="📄">
-            <Text delete style={styles.text}>
-              Clone or fork{" "}
+            <Text >
+              Download A Wallet{" "}
               <a
-                href="https://github.com/ethereum-boilerplate/ethereum-boilerplate#-quick-start"
+                href="https://verus.io/wallet"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                ethereum-boilerplate
+                Verus Wallet
               </a>{" "}
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="💿">
-            <Text delete style={styles.text}>
-              Install all dependencies: <Text code>npm install</Text>
+            <Text >
+              Grab A VerusID In Your Verus Wallet<Text code>Eg: friendlyNameSpace@ </Text>
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🧰">
-            <Text delete={isServerInfo} style={styles.text}>
-              Sign up for a free account on{" "}
+            <Text >
+              Sign Up For ID Watch{" "}
               <a
-                href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
+                href=""
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Moralis
+                SignUp
               </a>
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item dot="💾">
-            <Text delete={isServerInfo} style={styles.text}>
-              Create a Moralis Server (
-              <a
-                href="https://docs.moralis.io/moralis-server/getting-started/create-a-moralis-server"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                How to start Moralis Server
-              </a>
-              )
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item dot="🔏">
-            <Text delete={isServerInfo} style={styles.text}>
-              Rename <Text code>.env.example</Text> to <Text code>.env</Text> and provide your{" "}
-              <Text strong>appId</Text> and <Text strong>serverUrl</Text> from{" "}
-              <a
-                href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Moralis
-              </a>
-              :
-            </Text>
-            <Text code delete={isServerInfo} style={{ display: "block" }}>
-              REACT_APP_MORALIS_APPLICATION_ID = xxxxxxxxxxxx
-            </Text>
-            <Text code delete={isServerInfo} style={{ display: "block" }}>
-              REACT_APP_MORALIS_SERVER_URL = https://xxxxxx.grandmoralis.com:2053/server
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🔁">
-            <Text delete={isServerInfo} style={styles.text}>
-              Stop the app and start it again <Text code>npm run start</Text>
+          <Text >
+              Verify Your Login{" "}
+              <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Verify
+              </a>
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="💿">
             <Text delete={isInchDex} style={styles.text}>
-              Install{" "}
+              View Details{" "}
               <a
-                href="https://moralis.io/plugins/1inch/?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
+                href=""
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                1inch Moralis Plugin
+                Attestation
               </a>{" "}
-              needed for the<Text code>{"<InchDex />"}</Text> component (optional)
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🚀">
-            <Text style={styles.text}>BUIDL!!!</Text>
+            <Text style={styles.text}>Complete 👍!!!</Text>
           </Timeline.Item>
         </Timeline>
       </Card>
       <div>
         <Card
           style={styles.card}
-          title={<h1 style={styles.title}>💣 Starting Local Chain (optional)</h1>}
+          title={<h1 style={styles.title}>💣 Verus Mining & Staking</h1>}
         >
           <Timeline mode="left" style={styles.timeline}>
             <Timeline.Item dot="💿">
               <Text style={styles.text}>
-                Install{" "}
+                 Native Pools{" "}
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://www.npmjs.com/package/truffle"
+                  href=""
                 >
-                  Truffle
+                  Mining
                 </a>{" "}
                 and{" "}
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://www.npmjs.com/package/ganache-cli"
+                  href=""
                 >
-                  ganache-cli
+                  Staking
                 </a>{" "}
-                <Text code>npm install -g ganache-cli truffle</Text>
-              </Text>
-            </Timeline.Item>
-            <Timeline.Item dot="⚙️">
-              <Text style={styles.text}>
-                Start you local devchain: <Text code>npm run devchain</Text> on a new terminal
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="📡">
-              <Text style={styles.text}>
-                Deploy test contract: <Text code>npm run deploy</Text> on a new terminal
-              </Text>
-            </Timeline.Item>
-            <Timeline.Item dot="✅" style={styles.text}>
-              <Text>
-                Open the <Text strong>📄 Contract</Text> tab
+            <Text style={styles.text}>
+                 Veth Pools{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href=""
+                >
+                  Mining
+                </a>{" "}
+                and{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href=""
+                >
+                  Staking
+                </a>{" "}
               </Text>
             </Timeline.Item>
           </Timeline>
         </Card>
         <Card
           style={{ marginTop: "10px", ...styles.card }}
-          title={<h1 style={styles.title}>📡 Connecting your Local Chain to the Moralis DB</h1>}
+          title={<h1 style={styles.title}>📡 TO Chat Or Use PoolBot For Data</h1>}
         >
           <Timeline mode="left" style={styles.timeline}>
             <Timeline.Item dot="💿">
               <Text style={styles.text}>
-                Download{" "}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/fatedier/frp/releases"
-                >
-                  frpc
-                </a>{" "}
-                and provide missing params in the <Text code>.env</Text> file
+                The chat allows for use of bot and chatroom with other users
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="⚙️">
               <Text style={styles.text}>
-                Connect your Moralis Database and Local Chain: <Text code>npm run connect</Text>
+                Type ! followed by command to use bot <Text code>eg: !help</Text>
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="💾">
               <Text style={styles.text}>
-                Add contract events you want to watch: <Text code>npm run watch:events</Text>
+                Please Be Be mindful of others while using chat
+              </Text>
+            </Timeline.Item>
+            <Timeline.Item dot="💾">
+              <Text style={styles.text}>
+                Welcome
               </Text>
             </Timeline.Item>
           </Timeline>
